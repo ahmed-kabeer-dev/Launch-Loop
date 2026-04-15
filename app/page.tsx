1ef58d351c436b261c6481226f0291f1919fab00
ahmedkabeer <ahmedkabeer.x@gmail.com>
2026-04-15 13:03:30 +0530

    Featured Products

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import HeroSection from "@/components/ui/landing-page/hero-section";
+import FeaturedProducts from "@/components/ui/landing-page/featured-products";
+import RecentlyLaunchedProducts from "@/components/ui/landing-page/recently-launched-products";
 
 export default function Home() {
   return (
+    <div>
     <HeroSection />
-  
+    <FeaturedProducts />
+    <RecentlyLaunchedProducts />
+  </div>
   );
 }
